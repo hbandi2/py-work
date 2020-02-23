ff8a9adc9106eefb37f462d6895904dde67560b0
Harinatha Bandi <harinatha.bandi@gmail.com>
2020-02-23 15:19:44 -0600

    move to folder

---
+++ basics/variable-types-expression.py
@@ -0,0 +1,15 @@
+import sys
+
+i = 20
+j ='string value'
+k = 3.14
+
+print(sys.version)
+print('Integer '+ str(i) + ' String '+ j + ' Float ' + str(k))
+
+print('Your Name?')
+name = input()
+print('Your Age?')
+age = input()
+
+print('Your Name is ' + name + ' Age is ' + age)
